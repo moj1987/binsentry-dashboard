14c340eaf770dfa61fd6ddb573733444e8743925
Moj <49577315+moj1987@users.noreply.github.com>
2026-02-16 16:25:36 -0500

    Update bin

--- app/api/bins/route.js
+++ app/api/bins/route.js
@@ -1,4 +1,4 @@
-import { getBins} from '../../../lib/data'
+import { getBins, updateBin} from '../../../lib/data'
 
 export async function GET(){
     try{
@@ -6,5 +6,25 @@ export async function GET(){
         return Response.json(bins);
     }catch(error){
         return Response.json( {error: 'Failed to fetch bins'}, {status: 500})
+    }
+}
+
+
+export async function PUT(request){
+    try{
+        const {id, ...updateData} = await request.json();
+        if(!id){
+            return Response.json({ error: 'Missing item id' }, { status: 400 });
+        }
+
+        const result = await updateBin(id, updateData)
+
+        if (!result.success){
+            return Response.json({error: result.error}, {status:404})
+        }
+
+        return Response.json(result.data)
+    }catch(error){
+        return Response.json({error:'Failed to update bin'}, {status:500})
     }
 }
